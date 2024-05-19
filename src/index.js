7dd2e042f481e343b6d61ad7a64815668889e43e
gem-puneet <Puneet.Chaudhary@GeminiSolutions.com>
2024-05-19 17:43:02 +0530

    add faq component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import PackageButton from "./components/button/Button";
 import AnnouncementModal from "./components/announcementModal/AnnouncementModal";
 import AnnouncementTicker from "./components/announcementTicker/AnnouncementTicker";
+import FAQComponent from "./components/Faq/faq";
 
-export { PackageButton, AnnouncementModal, AnnouncementTicker };
+
+export { PackageButton, AnnouncementModal, AnnouncementTicker, FAQComponent };
