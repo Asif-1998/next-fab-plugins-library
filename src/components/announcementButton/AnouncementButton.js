f48851cbe18501f0824afef27076cbf3bcf5cdaa
Md Asif Momin <asif.momin@geminisolutions.com>
2024-05-20 15:22:37 +0530

    testing

---
+++ src/components/announcementButton/AnouncementButton.js
@@ -0,0 +1,8 @@
+import React from "react"
+import AnnouncementModal from "../announcementModal/AnnouncementModal"
+
+const AnnouncementButton = () => {
+    return <AnnouncementModal page="cart"/>
+}
+
+export default AnnouncementButton;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import AnnouncementModal from "./components/announcementModal/AnnouncementModal"
 import AnnouncementTicker from "./components/announcementTicker/AnnouncementTicker";
 import FAQGuide from "./components/Faq/faq";
 import TourGuide from "./components/tourGuide/TourGuide";
+import AnnouncementButton from "./components/announcementButton/AnouncementButton";
 
 
-export { PackageButton, AnnouncementModal, AnnouncementTicker, FAQGuide, TourGuide };
+export { PackageButton, AnnouncementModal, AnnouncementTicker, FAQGuide, TourGuide, AnnouncementButton };
