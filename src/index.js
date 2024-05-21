ad94085fa1cb66674ac7c112994975a80bfe9710
gem-puneet <Puneet.Chaudhary@GeminiSolutions.com>
2024-05-21 13:18:03 +0530

    remove driver js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import PackageButton from "./components/button/Button";
 //import AnnouncementModal from "./components/announcementModal/AnnouncementModal";
 import AnnouncementTicker from "./components/announcementTicker/AnnouncementTicker";
-import FAQGuide from "./components/Faq/faq";
 import TourGuide from "./components/tourGuide/TourGuide";
 import AnnouncementButton from "./components/announcementModal/AnnouncementModal";
 
 
-export { PackageButton, AnnouncementTicker, FAQGuide, TourGuide, AnnouncementButton };
+export { PackageButton, AnnouncementTicker, TourGuide, AnnouncementButton };
