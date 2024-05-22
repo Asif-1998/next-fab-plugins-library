50743176c14c0383e9b3ffe85b37c9cb384e9c28
gem-puneet <Puneet.Chaudhary@GeminiSolutions.com>
2024-05-22 11:08:24 +0530

    tour guide added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import PackageButton from "./components/button/Button";
 //import AnnouncementModal from "./components/announcementModal/AnnouncementModal";
 import AnnouncementTicker from "./components/announcementTicker/AnnouncementTicker";
 import FAQGuide from "./components/Faq/faq";
+import TourGuide from "./components/tourGuide/TourGuide";
 import AnnouncementButton from "./components/announcementModal/AnnouncementModal";
 
 
-export { PackageButton, AnnouncementTicker, FAQGuide, AnnouncementButton };
+export { PackageButton, AnnouncementTicker, FAQGuide, TourGuide, AnnouncementButton };
